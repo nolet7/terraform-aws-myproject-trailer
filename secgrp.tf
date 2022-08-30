4a0e8812ad4188b15952b238fb6b9f3571b6af70
lateef <nolet7@gmail.com>
2022-08-30 16:01:57 -0500

    updated security group

--- secgrp.tf
+++ secgrp.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "proj-bean-elb-sg" {
   name        = "proj1-bean-elb-sg"
   description = "security group for bean-elb"
-  vpc_id      = "module.vpc.vpc_id"
+  vpc_id      = module.vpc.vpc_id
   egress {
     from_port   = 0
     protocol    = "-1"
