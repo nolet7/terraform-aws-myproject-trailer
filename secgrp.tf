bdfb7cf9e82803a1acd528eec0ecf96d23935b57
lateef <nolet7@gmail.com>
2022-08-30 15:54:22 -0500

    updated security after error

--- secgrp.tf
+++ secgrp.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "proj-bean-elb-sg" {
   description = "security group for bean-elb"
   vpc_id      = "module.vpc.vpc_id"
   egress {
-    from_port  = 0
-    protocol   = "-1"
-    to_port    = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
@@ -16,62 +16,62 @@ resource "aws_security_group" "proj-bean-elb-sg" {
   }
 }
 resource "aws_security_group" "proj-bastion-sg" {
-  name = "proj1-bastion-sg"
+  name        = "proj1-bastion-sg"
   description = "security group for bastion ec2 instance"
-  vpc_id = module.vpc.vpc_id
-egress {
-  from_port = 0
-  protocol  = "-1"
-  to_port   = 0
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = [var.MYIP]
   }
 }
 resource "aws_security_group" "proj1-pord-sg" {
-  name = "proj1-prod-sg"
+  name        = "proj1-prod-sg"
   description = "security group for beanstalk instances"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
-    security_groups = [aws_security_group.proj-bastion-sg]
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
+    security_groups = [aws_security_group.proj-bastion-sg.id]
   }
 }
 resource "aws_security_group" "proj1-backend-sg" {
-  name = "proj1-backend-sg"
+  name        = "proj1-backend-sg"
   description = "security group for RDS,active mq,elastic cache"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    security_groups = [aws_security_group.proj1-pord-sg]
+    from_port       = 0
+    protocol        = "-1"
+    to_port         = 0
+    security_groups = [aws_security_group.proj1-pord-sg.id]
   }
 
-  }
+}
 resource "aws_security_group_rule" "sec-group-allow-itself" {
-  from_port         = 0
-  protocol          = "tcp"
-  security_group_id = aws_security_group.proj1-backend-sg
-  to_port           = 65535
-  type              = "ingress"
-  source_security_group_id = aws_security_group.proj1-backend-sg
+  from_port                = 0
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.proj1-backend-sg.id
+  to_port                  = 65535
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.proj1-backend-sg.id
 }
--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,16 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.VPC_NAME
-  cidr = var.vpcCIDR
-  azs = [var.zone1, var.zone2, var.zone3]
+  name            = var.VPC_NAME
+  cidr            = var.vpcCIDR
+  azs             = [var.zone1, var.zone2, var.zone3]
   private_subnets = [var.Privsub1CIDR, var.Privsub2CIDR, var.Privsub3CIDR]
-  public_subnets = [var.Pubsub1CIDR, var.Pubsub2CIDR, var.Pubsub3CIDR]
+  public_subnets  = [var.Pubsub1CIDR, var.Pubsub2CIDR, var.Pubsub3CIDR]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Terraform   = "true"
